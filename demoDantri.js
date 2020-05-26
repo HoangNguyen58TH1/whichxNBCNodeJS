7e15974f9ed32cfe0e1d209861e54ea32889b6ed
hoangnguyen <hoangnv20081998@gmail.com>
2020-05-27 01:20:24 +0700

    update small

--- demoDantri.js
+++ demoDantri.js
@@ -5,7 +5,8 @@ var WhichX = require("whichx");
     // Mở trình duyệt mới và tới trang của kenh14
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://tuoitre.vn/giao-duc.htm');
+    // await page.goto('https://tuoitre.vn/giao-duc.htm');
+    await page.goto('https://tuoitre.vn/kinh-doanh.htm');
 
     // Chạy đoạn JavaScript trong hàm này, đưa kết quả vào biến article
     const articles = await page.evaluate(() => {
@@ -30,8 +31,8 @@ var WhichX = require("whichx");
 
     category.addLabels(labels);
     category.addData("thể thao", "triệu tập ngôi sao tập luyện tập Ronaldo siêu phẩm cầu thủ chiêu mộ câu lạc bộ trận chấn thương VFF AFF tuyển world cup thái cực quyền đấm c.ronaldo sao trẻ man utd messi Văn Lâm dứt điểm dẫn dắt");
-    category.addData("giáo dục", "học sinh cổng trường trường trường học chủ tịch hiệu trưởng đại học THPT THCS tốt nghiệp điểm thi thi thử môn lãnh đạo xử lý trách nhiệm phụ huynh bằng khen");
-    category.addData("kinh doanh", "thu giữ xử lý bảo hiểm thị trường tập đoàn thuế giàu thiệt hại tỷ USD xuất khẩu bảo hộ y tế triệu triệu đồng tiêu dùng hàng hoá giảm giảm giá vốn đồng");
+    category.addData("giáo dục", "học sinh cổng trường trường trường học chủ tịch hiệu trưởng đại học THPT THCS tốt nghiệp điểm thi thi thử môn lãnh đạo xử lý trách nhiệm phụ huynh bằng khen cấp trên");
+    category.addData("kinh doanh", "thu giữ xử lý bảo hiểm thị trường tập đoàn thuế giàu thiệt hại tỷ USD xuất khẩu bảo hộ y tế triệu triệu đồng tiêu dùng hàng hoá giảm giảm giá vốn đồng công ty thủ tướng doanh nghiệp đầu tư suy thoái sòng bài quản lý hoạt động kinh doanh dịch vụ mua bán giá");
 
     for (let i = 0; i < articles.length; i++) {
       var News = category.classify(articles[i].title);
---
+++ jsonData.js
@@ -0,0 +1,93 @@
+[
+  {
+    title: 'Tổng thống Philippines không muốn mở cửa trường học khi chưa có vắcxin COVID-19',
+    url: '/tong-thong-philippines-khong-muon-mo-cua-truong-hoc-khi-chua-co-vacxin-covid-19-20200526190608676.htm'
+  },
+  {
+    title: "'Khi được cấp trên bố trí, không làm sẽ không tồn tại'",
+    url: '/khi-duoc-cap-tren-bo-tri-khong-lam-se-khong-ton-tai-20200526181919267.htm'
+  },
+  {
+    title: "'Nhiều phụ huynh muốn làm trường quốc tế mất uy tín'",
+    url: '/nhieu-phu-huynh-muon-lam-truong-quoc-te-mat-uy-tin-20200526174147965.htm'
+  },
+  {
+    title: 'Tranh chấp hội đồng quản trị Đại học Hoa Sen: 5 năm chưa thể xét xử',
+    url: '/tranh-chap-hoi-dong-quan-tri-dai-hoc-hoa-sen-5-nam-chua-the-xet-xu-20200526165904989.htm'
+  },
+  {
+    title: 'Học sinh tử vong do cây đổ: Mẹ của em mới sinh con nhỏ 3 ngày',
+    url: '/hoc-sinh-tu-vong-do-cay-do-me-cua-em-moi-sinh-con-nho-3-ngay-20200526143425227.htm'
+  },
+  {
+    title: 'Bộ trưởng Phùng Xuân Nhạ yêu cầu rà soát cây xanh trong trường học',
+    url: '/bo-truong-phung-xuan-nha-yeu-cau-ra-soat-cay-xanh-trong-truong-hoc-20200526145731368.htm'
+  },
+  {
+    title: "'Cõng' sách lên bản làm thư viện",
+    url: '/cong-sach-len-ban-lam-thu-vien-20200525212910346.htm'
+  },
+  {
+    title: 'Hoa hậu Trần Tiểu Vy tham gia “phiên chợ 0 đồng” giúp sinh viên khó khăn',
+    url: '/hoa-hau-tran-tieu-vy-tham-gia-phien-cho-0-dong-giup-sinh-vien-kho-khan-20200526110808379.htm'
+  },
+  {
+    title: 'Hiệu phó nhờ giáo viên làm thay bài kiểm tra khi học đại học',
+    url: '/hieu-pho-nho-giao-vien-lam-thay-bai-kiem-tra-khi-hoc-dai-hoc-20200526093252092.htm'
+  },
+  {
+    title: 'Thi tốt nghiệp THPT 2020: Đại học tham gia thanh tra coi thi, chấm thi',
+    url: '/thi-tot-nghiep-thpt-2020-dai-hoc-tham-gia-thanh-tra-coi-thi-cham-thi-20200525202353242.htm'
+  },
+  {
+    title: 'Luật sư yêu cầu truy cứu cựu giám đốc Sở Giáo dục trong vụ nâng điểm Sơn La',
+    url: '/luat-su-yeu-cau-truy-cuu-cuu-giam-doc-so-giao-duc-trong-vu-nang-diem-son-la-20200525182827858.htm'
+  },
+  {
+    title: "Đưa tiền nhờ nâng điểm vì 'muốn con được học hành đến nơi đến chốn'",
+    url: '/dua-tien-nho-nang-diem-vi-muon-con-duoc-hoc-hanh-den-noi-den-chon-20200525163109846.htm'
+  },
+  {
+    title: 'Học sinh lớp 9 bị điện giật chết khi cắt tỉa cây ở trường',
+    url: '/hoc-sinh-lop-9-bi-dien-giat-chet-khi-cat-tia-cay-o-truong-2020052515221999.htm'
+  },
+  {
+    title: 'Hà Nội công bố chỉ tiêu tuyển sinh lớp 10 các trường công lập',
+    url: '/ha-noi-tuyen-66-492-hoc-sinh-vao-lop-10-cac-truong-thpt-cong-lap-20200525154359705.htm'
+  },
+  {
+    title: 'Học sinh mầm non TP.HCM trở lại học: Sĩ số thưa thớt do trời mưa?',
+    url: '/hoc-sinh-mam-non-tp-hcm-tro-lai-hoc-si-so-thua-thot-do-troi-mua-20200525125514966.htm'
+  }
+]
+
+Tổng thống Philippines không muốn mở cửa trường học khi chưa có vắcxin COVID-19
+It is: 0 giáo dục
+'Khi được cấp trên bố trí, không làm sẽ không tồn tại'
+It is: 1 thể thao
+'Nhiều phụ huynh muốn làm trường quốc tế mất uy tín'
+It is: 2 giáo dục
+Tranh chấp hội đồng quản trị Đại học Hoa Sen: 5 năm chưa thể xét xử
+It is: 3 kinh doanh
+Học sinh tử vong do cây đổ: Mẹ của em mới sinh con nhỏ 3 ngày
+It is: 4 giáo dục
+Bộ trưởng Phùng Xuân Nhạ yêu cầu rà soát cây xanh trong trường học
+It is: 5 giáo dục
+'Cõng' sách lên bản làm thư viện
+It is: 6 giáo dục
+Hoa hậu Trần Tiểu Vy tham gia “phiên chợ 0 đồng” giúp sinh viên khó khăn
+It is: 7 giáo dục
+Hiệu phó nhờ giáo viên làm thay bài kiểm tra khi học đại học
+It is: 8 giáo dục
+Thi tốt nghiệp THPT 2020: Đại học tham gia thanh tra coi thi, chấm thi
+It is: 9 giáo dục
+Luật sư yêu cầu truy cứu cựu giám đốc Sở Giáo dục trong vụ nâng điểm Sơn La
+It is: 10 thể thao
+Đưa tiền nhờ nâng điểm vì 'muốn con được học hành đến nơi đến chốn'
+It is: 11 giáo dục
+Học sinh lớp 9 bị điện giật chết khi cắt tỉa cây ở trường
+It is: 12 giáo dục
+Hà Nội công bố chỉ tiêu tuyển sinh lớp 10 các trường công lập
+It is: 13 giáo dục
+Học sinh mầm non TP.HCM trở lại học: Sĩ số thưa thớt do trời mưa?
+It is: 14 giáo dục
